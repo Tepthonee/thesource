0ad31533b0c825d4e1bd4cc53c680270d95b6ecc
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2024-07-20 20:48:47 +0300

    Update __init__.py

--- Tepthon/sql_helper/__init__.py
+++ Tepthon/sql_helper/__init__.py
@@ -1,5 +1,4 @@
 import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -9,26 +8,19 @@ from ..Config import Config
 from ..core.logger import logging
 
 LOGS = logging.getLogger(__name__)
-
+engine = None
+BASE = declarative_base()
 
 def start() -> scoped_session:
-    database_url = (
-        Config.DB_URI.replace("postgres:", "postgresql:")
-        if "postgres://" in Config.DB_URI
-        else Config.DB_URI
-    )
-    engine = create_engine(database_url)
+    global engine
+    engine = create_engine(Config.DB_URI)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
-
 try:
-    BASE = declarative_base()
     SESSION = start()
 except AttributeError as e:
     # this is a dirty way for the work-around required for #23
-    LOGS.error(
-        "DB_URI is not configured. Features depending on the database might have issues."
-    )
+    LOGS.error("DB_URI is not configured. Features depending on the database might not work.")
     LOGS.error(str(e))
