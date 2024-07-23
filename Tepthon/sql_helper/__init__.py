56a3e2f0946e73261d19df18a6a4c18a1ae7cbda
Mohammad Taleb <98346135+RRYR7@users.noreply.github.com>
2024-07-23 21:48:17 +0300

    Update __init__.py

--- Tepthon/sql_helper/__init__.py
+++ Tepthon/sql_helper/__init__.py
@@ -1,19 +1,9 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# CatUserBot #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-# Copyright (C) 2020-2023 by TgCatUB@Github.
-
-# This file is part of: https://github.com/TgCatUB/catuserbot
-# and is released under the "GNU v3.0 License Agreement".
-
-# Please see: https://github.com/TgCatUB/catuserbot/blob/master/LICENSE
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
 import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-# the secret configuration specific things
 from ..Config import Config
 from ..core.logger import logging
 
