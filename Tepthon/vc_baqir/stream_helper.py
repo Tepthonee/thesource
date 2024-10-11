3956223829f4698513e35ab85d2ad770c2774da8
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2024-10-11 18:06:20 +0300

    Update stream_helper.py

--- Tepthon/vc_baqir/stream_helper.py
+++ Tepthon/vc_baqir/stream_helper.py
@@ -1,5 +1,9 @@
 import re
 import os
+import random
+import glob
+import random
+import io
 try:
     import enum
 except ModuleNotFoundError:
@@ -18,10 +22,18 @@ class Stream(Enum):
     video = 2
 
 
+def get_cookies_file():
+    folder_path = f"{os.getcwd()}/zion"
+    txt_files = glob.glob(os.path.join(folder_path, '*.txt'))
+    if not txt_files:
+        raise FileNotFoundError("No .txt files found in the specified folder.")
+    cookie_txt_file = random.choice(txt_files)
+    return cookie_txt_file
+
+
 yt_regex_str = "^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
 
 yt_regex = re.compile(yt_regex_str)
-
 
 def check_url(url):
     prepared_request = PreparedRequest()
@@ -35,15 +47,15 @@ def check_url(url):
 async def get_yt_stream_link(url, audio_only=False):
     if audio_only:
         return (
-            await runcmd(f"yt-dlp --no-warnings --geo-bypass -f bestaudio -g {url}")
+            await runcmd(f"yt-dlp --cookies {get_cookies_file()} --geo-bypass -f bestaudio -g {url}")
         )[0]
-    return (await runcmd(f"yt-dlp --no-warnings --geo-bypass -f best -g {url}"))[0]
+    return (await runcmd(f"yt-dlp --cookies {get_cookies_file()} --geo-bypass -f bestvideo -g {url}"))[0]
 
 
 async def video_dl(url, title):
     path = f"temp/{title.replace(' ', '_')}.mp4"
     video_opts = {
-        "format": "best",
+        "format": "(bestvideo[height<=?360][ext=mp4])+(bestaudio[ext=m4a])",
         "addmetadata": True,
         "key": "FFmpegMetadata",
         "writethumbnail": False,
@@ -57,6 +69,8 @@ async def video_dl(url, title):
         "outtmpl": path,
         "logtostderr": False,
         "quiet": True,
+        "no_warnings": True,
+        "cookiefile" : get_cookies_file(),
     }
 
     with YoutubeDL(video_opts) as ytdl:
