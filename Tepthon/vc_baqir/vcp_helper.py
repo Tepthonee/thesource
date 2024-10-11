e2779c01dcc38a21d25a6080278d8449ac98eaff
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2024-10-11 18:08:42 +0300

    Update vcp_helper.py

--- Tepthon/vc_baqir/vcp_helper.py
+++ Tepthon/vc_baqir/vcp_helper.py
@@ -1,7 +1,10 @@
 import asyncio
 from pathlib import Path
-
+import os
+import glob
+import random
 import requests
+
 from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.exceptions import (
     AlreadyJoinedError,
@@ -14,12 +17,24 @@ from pytgcalls.types import AudioPiped, AudioVideoPiped
 from pytgcalls.types.stream import StreamAudioEnded
 from telethon import functions
 from telethon.errors import ChatAdminRequiredError
+from telethon.errors.rpcerrorlist import ChannelInvalidError
 from yt_dlp import YoutubeDL
 
 from .stream_helper import Stream, check_url, video_dl, yt_regex
+from ..Config import Config
+
+vc_session = Config.VC_SESSION
+
+def get_cookies_file():
+    folder_path = f"{os.getcwd()}/zion"
+    txt_files = glob.glob(os.path.join(folder_path, '*.txt'))
+    if not txt_files:
+        raise FileNotFoundError("No .txt files found in the specified folder.")
+    cookie_txt_file = random.choice(txt_files)
+    return cookie_txt_file
 
 
-class TepVC:
+class ZedVC:
     def __init__(self, client) -> None:
         self.app = PyTgCalls(client, overload_quiet_mode=True)
         self.client = client
@@ -43,13 +58,21 @@ class TepVC:
 
     async def join_vc(self, chat, join_as=None):
         if self.CHAT_ID:
-            return f"**- مـوجـود بالفعـل بالمحـادثـه الصـوتيـه عـلى** {self.CHAT_NAME}"
+            try:
+                await self.app.leave_group_call(self.CHAT_ID)
+            except (NotInGroupCallError, NoActiveGroupCall):
+                pass
+            self.CHAT_NAME = None
+            self.CHAT_ID = None
+            self.PLAYING = False
+            self.PLAYLIST = []
+            #return f"⚈ **مـوجـود بالفعـل بالمحـادثـه الصـوتيـه عـلى** {self.CHAT_NAME}"
         if join_as:
             try:
                 join_as_chat = await self.client.get_entity(int(join_as))
                 join_as_title = f" كـ **{join_as_chat.title}**"
             except ValueError:
-                return "**- قم باضافة ايدي المجموعه لامر الانضمام**"
+                return "⚈ **قم باضافة ايدي المجموعه لامر الانضمام**"
         else:
             join_as_chat = await self.client.get_me()
             join_as_title = ""
@@ -61,16 +84,30 @@ class TepVC:
                 stream_type=StreamType().pulse_stream,
             )
         except NoActiveGroupCall:
-            try:
-                await self.client(
-                    functions.phone.CreateGroupCallRequest(
-                        peer=chat,
-                        title="TepVC",
+            if vc_session:
+                try:
+                    await self.client(
+                        functions.phone.CreateGroupCallRequest(
+                            peer=chat,
+                            title="مكالمة صوتيه",
+                        )
                     )
-                )
-                await self.join_vc(chat=chat, join_as=join_as)
-            except ChatAdminRequiredError:
-                return "**- انت بحاجـه الى صلاحيـات المشـرف لبـدء محـادثه صـوتيـه, او قم بطلـب من احـد المشـرفين**"
+                    await self.join_vc(chat=chat, join_as=join_as)
+                except ChatAdminRequiredError:
+                    return "⚉ **انت بحاجه الى صلاحيات المشـرف✖️**\n⚉ **لـ بـدء محـادثه صـوتيـه هنـا 🤷🏻‍♀**\n⚉ **او قم بطلب من احـد المشـرفين هنـا**"
+                except ChannelInvalidError:
+                    return "⚉ **لديك حساب مساعد للميوزك قمت بتعيينه سابقاً**\n⚉قم باضافة الحساب المساعد اولاً للمجموعة**\n⚉ **ثم قم بفتح المكالمه في المجموعة**"
+            else:
+                try:
+                    await self.client(
+                        functions.phone.CreateGroupCallRequest(
+                            peer=chat,
+                            title="مكالمة صوتيه",
+                        )
+                    )
+                    await self.join_vc(chat=chat, join_as=join_as)
+                except ChatAdminRequiredError:
+                    return "⚉ **انت بحاجه الى صلاحيات المشـرف✖️**\n⚉ **لـ بـدء محـادثه صـوتيـه هنـا 🤷🏻‍♀**\n⚉ **او قم بطلب من احـد المشـرفين هنـا**"
         except (NodeJSNotInstalled, TooOldNodeJSVersion):
             return "- آخـر اصـدار من NodeJs لم يتـم تحميلـه ...؟!"
         except AlreadyJoinedError:
@@ -79,7 +116,10 @@ class TepVC:
             await self.join_vc(chat=chat, join_as=join_as)
         self.CHAT_ID = chat.id
         self.CHAT_NAME = chat.title
-        return f"**- تم الانضمـام بنجـاح الى المحادثـه الصـوتيـه** **{chat.title}**{join_as_title}"
+        if vc_session:
+            return f"⚉ **تم الانضمـام بنجـاح ✓**\n⚉ **الى المكالمـة ⤌ ⎞** {chat.title} **⎝** - {join_as_title}\n⚉ **الانضمام:** عبر الحساب المساعـد"
+        else:
+            return f"⚉ **تم الانضمـام بنجـاح ✓**\n⚉ **الى المكالمـة ⤌ ⎞** {chat.title} **⎝** - {join_as_title}"
 
     async def leave_vc(self):
         try:
@@ -93,19 +133,19 @@ class TepVC:
 
     async def play_song(self, input, stream=Stream.audio, force=False):
         if yt_regex.match(input):
-            with YoutubeDL({}) as ytdl:
+            with YoutubeDL({"no-playlist": True, "cookiefile": get_cookies_file()}) as ytdl:
                 ytdl_data = ytdl.extract_info(input, download=False)
                 title = ytdl_data.get("title", None)
             if title:
                 playable = await video_dl(input, title)
             else:
-                return "**- خطـأ بجلب الرابـط**"
+                return "⚈ **خطـأ بجلب الرابـط**"
         elif check_url(input):
             try:
                 res = requests.get(input, allow_redirects=True, stream=True)
                 ctype = res.headers.get("Content-Type")
                 if "video" not in ctype or "audio" not in ctype:
-                    return "- رابـط غيـر صـالح ؟!"
+                    return "⚈ **رابـط غيـر صالـح ✘**"
                 name = res.headers.get("Content-Disposition", None)
                 if name:
                     title = name.split('="')[0].split('"') or ""
@@ -113,32 +153,38 @@ class TepVC:
                     title = input
                 playable = input
             except Exception as e:
-                return f"**- رابـط غيـر صـالح :**\n\n{e}"
+                return f"⚈ **رابـط غيـر صـالح :**\n\n{e}"
         else:
             path = Path(input)
             if path.exists():
                 if not path.name.endswith(
                     (".mkv", ".mp4", ".webm", ".m4v", ".mp3", ".flac", ".wav", ".m4a")
                 ):
-                    return "**- ملف غيـر صـالح لتشغيـله**"
+                    return "⚈ **ملف غيـر صـالح لتشغيـله**"
                 playable = str(path.absolute())
                 title = path.name
             else:
-                return "**- مسـار الملـف غيـر موجـود ؟!**"
+                return "⚈ **مسـار الملـف غيـر موجـود ؟!**"
         print(playable)
         if self.PLAYING and not force:
             self.PLAYLIST.append({"title": title, "path": playable, "stream": stream})
-            return f"- تم الاضـافه لـ قـائمـة التشغيـل ✓\n- المـوقـع: {len(self.PLAYLIST)+1}"
+            return f"⚈ **تم الاضـافه لـ قـائمـة التشغيـل ✓**\n⚈ **المـوقـع:** {len(self.PLAYLIST)+1}"
         if not self.PLAYING:
             self.PLAYLIST.append({"title": title, "path": playable, "stream": stream})
             await self.skip()
-            return f"**- جـارِ تشغيـل** {title}"
+            if vc_session:
+                return f"⚉ **تم التشغيـل .. بنجـاح 🎶**\n⚉ **العنـوان:** `{title}`\n⚉ **التشغيـل:** عبر الحساب المساعـد\n⚉ **لـ عـرض اوامـر الميـوزك ⇜⎞** `.ميوزك` **⎝**"
+            else:
+                return f"⚉ **تم التشغيـل .. بنجـاح 🎶**\n⚉ **العنـوان:** `{title}`\n⚉ **لـ عـرض اوامـر الميـوزك ⇜⎞** `.ميوزك` **⎝**"
         if force and self.PLAYING:
             self.PLAYLIST.insert(
                 0, {"title": title, "path": playable, "stream": stream}
             )
             await self.skip()
-            return f"**- جـارِ تشغيـل** {title}"
+            if vc_session:
+                return f"⚉ **تم التشغيـل .. بنجـاح 🎶**\n⚉ **العنـوان:** `{title}`\n⚉ **التشغيـل:** عبر الحساب المساعـد\n⚉ **لـ عـرض اوامـر الميـوزك ⇜⎞** `.ميوزك` **⎝**"
+            else:
+                return f"⚉ **تم التشغيـل .. بنجـاح 🎶**\n⚉ **العنـوان:** `{title}`\n⚉ **لـ عـرض اوامـر الميـوزك ⇜⎞** `.ميوزك` **⎝**"
 
     async def handle_next(self, update):
         if isinstance(update, StreamAudioEnded):
@@ -155,7 +201,7 @@ class TepVC:
                     AudioPiped("zedthon/ahmed/Silence01s.mp3"),
                 )
             self.PLAYING = False
-            return "**- التخطـي:\nقائمـة الشغيـل فارغـه ؟!**"
+            return "⚈ **التخطـي ➰**\n⚈ **عـذراً عـزيـزي ✗**\n⚈ **قائمـة الشغيـل فارغـه ؟!**"
 
         next = self.PLAYLIST.pop(0)
         if next["stream"] == Stream.audio:
@@ -167,23 +213,26 @@ class TepVC:
         except Exception:
             await self.skip()
         self.PLAYING = next
-        return f"**- تم التخطي\n- جـارِ تشغيـل :** `{next['title']}`"
+        if vc_session:
+            return f"⚈ **تم التخطـي ➰**\n⚉ **تم تشغيـل التالي .. بنجـاح 🎶**\n⚉ **العنـوان:** `{next['title']}`\n⚉ **التشغيـل:** عبر الحساب المساعـد\n\n⚉ **لـ عـرض اوامـر الميـوزك ⇜⎞** `.ميوزك` **⎝**"
+        else:
+            return f"⚈ **تم التخطـي ➰**\n⚉ **تم تشغيـل التالي .. بنجـاح 🎶**\n⚉ **العنـوان:** `{next['title']}`\n\n⚉ **لـ عـرض اوامـر الميـوزك ⇜⎞** `.ميوزك` **⎝**"
 
     async def pause(self):
         if not self.PLAYING:
-            return "**- لايـوجـد شـي لـ الايقـاف ؟!**"
+            return "⚈ **عـذراً عـزيـزي ✗**\n⚈ **لايـوجـد شـي لـ الايقـاف ؟!**"
         if not self.PAUSED:
             await self.app.pause_stream(self.CHAT_ID)
             self.PAUSED = True
-        return f"**- تم التمهـل في** {self.CHAT_NAME}"
+        return f"⚈ **تم التمهـل في** {self.CHAT_NAME}"
 
     async def resume(self):
         if not self.PLAYING:
-            return "**- لايـوجـد شـي لـ الاستئنـاف ؟!**"
+            return "⚈ **عـذراً عـزيـزي ✗**\n⚈ **لايـوجـد شـي لـ الاستئنـاف ؟!**"
         if self.PAUSED:
             await self.app.resume_stream(self.CHAT_ID)
             self.PAUSED = False
-        return f"**- تم الاستئنـاف في**  {self.CHAT_NAME}"
+        return f"⚈ **تم الاستئنـاف في**  {self.CHAT_NAME}"
 
     # async def mute(self):
     #     if not self.PLAYING:
