82810e4b6c4f72b6b35cdeba0622f6e983cdf815
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2024-11-13 06:34:13 +0300

    Update __init__.py

--- Tepthon/__init__.py
+++ Tepthon/__init__.py
@@ -1,41 +1,30 @@
-import signal
-import sys
 import time
 
 import heroku3
 
 from .Config import Config
 from .core.logger import logging
-from .core.session import zedub, tgbot
-from .helpers.functions.converter import Convert
-from .helpers.functions.musictool import *
-from .helpers.utils.utils import runasync
+from .core.session import zedub
 from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
 __version__ = "3.2.0"
-__license__ = "GNU Affero General Public License v3.0"
-__author__ = "Tepthon <https://github.com/Tepthonee/thesource>"
-__copyright__ = "Tepthon Copyright (C) 2021 - 2022  {__author__}"
+__license__ = "تعديل فريق تيبثون"
+__author__ = "تيبثون <https://t.me/Tepthon>"
+__copyright__ = "TEPTHON TEAM (C) 2021 - 2024  " + __author__
 
 zedub.version = __version__
 zedub.tgbot.version = __version__
-LOGS = logging.getLogger("تيبثـون")
+LOGS = logging.getLogger("jepthon")
 bot = zedub
 
-
 StartTime = time.time()
-tepversion = "3.1.1"
+zedversion = "3.2.0"
 
 
-def close_connection(*_):
-    print("تم إغلاق الاتصال بالسورس")
-    runasync(zedub.disconnect())
-    sys.exit(143)
-
-
-signal.signal(signal.SIGTERM, close_connection)
-
-UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
+if Config.UPSTREAM_REPO == "Tepthonee":
+    UPSTREAM_REPO_URL = "https://github.com/Tepthonee/thesource"
+else:
+    UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
 
 if Config.PRIVATE_GROUP_BOT_API_ID == 0:
     if gvarstatus("PRIVATE_GROUP_BOT_API_ID") is None:
@@ -47,7 +36,7 @@ if Config.PRIVATE_GROUP_BOT_API_ID == 0:
         Config.BOTLOG = True
 else:
     if str(Config.PRIVATE_GROUP_BOT_API_ID)[0] != "-":
-        Config.BOTLOG_CHATID = int(f"-{str(Config.PRIVATE_GROUP_BOT_API_ID)}")
+        Config.BOTLOG_CHATID = int("-" + str(Config.PRIVATE_GROUP_BOT_API_ID))
     else:
         Config.BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
     Config.BOTLOG = True
@@ -58,8 +47,7 @@ if Config.PM_LOGGER_GROUP_ID == 0:
     else:
         Config.PM_LOGGER_GROUP_ID = int(gvarstatus("PM_LOGGER_GROUP_ID"))
 elif str(Config.PM_LOGGER_GROUP_ID)[0] != "-":
-    Config.PM_LOGGER_GROUP_ID = int(f"-{str(Config.PM_LOGGER_GROUP_ID)}")
-
+    Config.PM_LOGGER_GROUP_ID = int("-" + str(Config.PM_LOGGER_GROUP_ID))
 try:
     if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
         HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[
@@ -89,3 +77,5 @@ LOAD_PLUG = {}
 BOTLOG = Config.BOTLOG
 BOTLOG_CHATID = Config.BOTLOG_CHATID
 PM_LOGGER_GROUP_ID = Config.PM_LOGGER_GROUP_ID
+
+From the joker
