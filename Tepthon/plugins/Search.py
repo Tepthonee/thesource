4d538af3388da707caac3dffa4f2e920babcf980
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2025-01-23 20:29:02 +0300

    Update Search.py

--- Tepthon/plugins/Search.py
+++ Tepthon/plugins/Search.py
@@ -20,6 +20,15 @@ from . import zedub, song_download
 plugin_category = "البحث"
 LOGS = logging.getLogger(__name__)
 
+
+def get_cookies_file():
+    folder_path = f"{os.getcwd()}/rcookies"
+    txt_files = glob.glob(os.path.join(folder_path, '*.txt'))
+    if not txt_files:
+        raise FileNotFoundError("No .txt files found in the specified folder.")
+    cookie_txt_file = random.choice(txt_files)
+    return cookie_txt_file
+    
 # =========================================================== #
 #                                                             𝙕𝙏𝙝𝙤𝙣
 # =========================================================== #
