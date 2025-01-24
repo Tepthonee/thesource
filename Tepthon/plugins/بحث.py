b413a71fe175cc760c9e9db91c8910ccc480f934
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2025-01-24 15:03:10 +0300

    Update بحث.py

--- Tepthon/plugins/بحث.py
+++ Tepthon/plugins/بحث.py
@@ -5,6 +5,7 @@ import yt_dlp
 import os
 from telethon import TelegramClient, events
 from yt_dlp import YoutubeDL
+from Tepthon import zedub
 from ..Config import Config
 
 plugin_category = "البوت"
