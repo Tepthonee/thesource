fdb611945c542d824cb7f67a0ba2aeaede3754e5
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2025-01-25 20:42:00 +0300

    Update إنستا.py

--- Tepthon/plugins/إنستا.py
+++ Tepthon/plugins/إنستا.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 from telethon import events
 from Tepthon import zedub
 from ..Config import Config
+import json
+import os
 
 plugin_category = "البوت"
 
@@ -16,13 +18,33 @@ async def download_video(event):
 
     try:
         response = requests.get(video_url)
+        if response.status_code != 200:
+            await event.reply("فشل في الوصول إلى الرابط. تأكد من أنه صالح.")
+            return
+        
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # العثور على عنصر الفيديو
-        video_tag = soup.find('video')
-        if video_tag and video_tag.get('src'):
-            video_src = video_tag['src']
-            video_response = requests.get(video_src)
+        # البحث عن السكربت الذي يحتوي على البيانات
+        scripts = soup.find_all('script')
+        video_url = None
+
+        for script in scripts:
+            if 'window._sharedData =' in str(script):
+                shared_data = str(script).split('window._sharedData = ')[1].split(';</script>')[0]
+                json_data = json.loads(shared_data)
+
+                # البحث عن فيديو إذا كان موجودًا
+                try:
+                    media = json_data['entry_data']['PostPage'][0]['graphql']['shortcode_media']
+                    if media['is_video']:
+                        video_url = media['video_url']
+                except KeyError:
+                    continue
+
+                break
+
+        if video_url:
+            video_response = requests.get(video_url)
             filename = 'video.mp4'
 
             with open(filename, 'wb') as f:
@@ -35,5 +57,6 @@ async def download_video(event):
             os.remove(filename)
         else:
             await event.reply("لم أتمكن من العثور على رابط الفيديو.")
+
     except Exception as e:
         await event.reply(f"حدث خطأ أثناء تحميل الفيديو: {e}")
