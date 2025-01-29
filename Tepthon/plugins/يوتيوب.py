97668b8839eaa010ce91e75f25683f0c57e78c6f
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2025-01-29 22:24:42 +0300

    Update يوتيوب.py

--- Tepthon/plugins/يوتيوب.py
+++ Tepthon/plugins/يوتيوب.py
@@ -154,8 +154,8 @@ async def fix_attributes(
 
 
 @zedub.zed_cmd(
-    pattern="تحميل صوت(?:\s|$)([\s\S]*)",
-    command=("تحميل صوت", plugin_category),
+    pattern="صوت(?:\s|$)([\s\S]*)",
+    command=("صوت", plugin_category),
     info={
         "header": "تحميـل الاغـاني مـن يوتيوب .. فيسبوك .. انستا .. الـخ عـبر الرابـط",
         "مثــال": ["{tr}تحميل صوت بالــرد ع رابــط", "{tr}تحميل صوت + رابــط"],
@@ -684,8 +684,8 @@ async def download_audio(event):
 
 
 @zedub.zed_cmd(
-    pattern="انستا(?: |$)([\s\S]*)",
-    command=("انستا", plugin_category),
+    pattern="مضيوف ..(?: |$)([\s\S]*)",
+    command=("مضيوف ..", plugin_category),
     info={
         "header": "لـ تحميـل الصـور والفيـديـو مـن الانستـا",
         "مثــال": [
@@ -835,51 +835,81 @@ async def yt_search(event):
     reply_text = f"**⎉╎اليك عزيزي قائمة بروابط الكلمة اللتي بحثت عنها:**\n`{query}`\n\n**⎉╎النتائج:**\n{full_response}"
     await edit_or_reply(video_q, reply_text)
 
+
 @zedub.zed_cmd(
-    pattern="ستوري(?: |$)(\d*)? ?([\s\S]*)",
-    command=("ستوري", plugin_category),
+    pattern="بحث(?:\ع|$)([\s\S]*)",
+    command=("بحث", plugin_category),
     info={
-        "header": "لـ تـحـميل ستـوري",
-        "مثــال": [
-            "{tr}ستوري + الرابط",
+        "header": "To reverse search song.",
+        "الوصـف": "Reverse search audio file using shazam api",
+        "امـر مضـاف": {"ع": "To send the song of sazam match"},
+        "الاستخـدام": [
+            "{tr}ابحث بالــرد ع بصمـه او مقطـع صوتي",
+            "{tr}ابحث ع بالــرد ع بصمـه او مقطـع صوتي",
         ],
     },
 )
-async def _(event):
-    if event.fwd_from:
-        return
-    rnryr_link = event.pattern_match.group(1)
-    chat = "@msaver_bot"
+async def shazamcmd(event):
+    "To reverse search song."
+    reply = await event.get_reply_message()
+    mediatype = await media_type(reply)
+    chat = "@DeezerMusicBot"
+    delete = False
+    flag = event.pattern_match.group(1)
+    if not reply or not mediatype or mediatype not in ["Voice", "Audio"]:
+        return await edit_delete(
+            event, "**- بالــرد ع مقطـع صـوتي**"
+        )
+    zedevent = await edit_or_reply(event, "**- جـار تحميـل المقـطع الصـوتي ...**")
+    name = "zed.mp3"
+    try:
+        for attr in getattr(reply.document, "attributes", []):
+            if isinstance(attr, types.DocumentAttributeFilename):
+                name = attr.file_name
+        dl = io.FileIO(name, "a")
+        await event.client.fast_download_file(
+            location=reply.document,
+            out=dl,
+        )
+        dl.close()
+        mp3_fileto_recognize = open(name, "rb").read()
+        shazam = Shazam(mp3_fileto_recognize)
+        recognize_generator = shazam.recognizeSong()
+        track = next(recognize_generator)[1]["track"]
+    except Exception as e:
+        LOGS.error(e)
+        return await edit_delete(
+            zedevent, f"**- خطـأ :**\n__{e}__"
+        )
+
+    file = track["images"]["background"]
+    title = track["share"]["subject"]
+    slink = await yt_search(title)
+    if flag == "s":
+        deezer = track["hub"]["providers"][1]["actions"][0]["uri"][15:]
         async with event.client.conversation(chat) as conv:
-        except YouBlockedUserError:
-            await event.edit("⎉╎ فـك حـظر البـوت وحـاول مجـددا @msaver_bot")
-            return
-        chat = await conv.send_message("/start")
-        checker = await conv.get_response()
-        await event.client.send_read_acknowledge(conv.chat_id)
-        if "Choose the language you like" in checker.message:
-            await checker.click(1)
-            await conv.send_message(rnryr_link)
+            try:
+                purgeflag = await conv.send_message("/start")
+            except YouBlockedUserError:
+                await zedub(unblock("DeezerMusicBot"))
+                purgeflag = await conv.send_message("/start")
             await conv.get_response()
             await event.client.send_read_acknowledge(conv.chat_id)
-        await conv.send_message(rnryr_link)
-        await conv.get_response()
-        await event.client.send_read_acknowledge(conv.chat_id)
-        media = await conv.get_response(timeout=10)
+            await conv.send_message(deezer)
+            await event.client.get_messages(chat)
+            song = await event.client.get_messages(chat)
+            await song[0].click(0)
+            await conv.get_response()
+            file = await conv.get_response()
             await event.client.send_read_acknowledge(conv.chat_id)
-            if media.media:
-                while True:
-                    media_list.append(media)
-                    try:
-                        media = await conv.get_response(timeout=2)
-                        await event.client.send_read_acknowledge(conv.chat_id)
-                    except asyncio.TimeoutError:
-                        break
-                details = media_list[0].message.splitlines()
-                await zedevent.delete()
-                await event.client.send_file(
-                    event.chat_id,
-                    media_list,
-                    caption=f"**{details[0]}**",
-                )
-                return await delete_conv(event, chat)
+            delete = True
+    await event.client.send_file(
+        event.chat_id,
+        file,
+        caption=f"<b>⎉╎ المقطـع الصـوتي :</b> <code>{title}</code>\n<b>⎉╎ الرابـط : <a href = {slink}/1>YouTube</a></b>",
+        reply_to=reply,
+        parse_mode="html",
+    )
+    await zedevent.delete()
+    if delete:
+        await delete_conv(event, chat, 
