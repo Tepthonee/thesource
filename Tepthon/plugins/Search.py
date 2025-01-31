3fca93eadbeca0bb4b855523ce6f232fd2262a2a
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2025-01-31 14:41:44 +0300

    Update Search.py

--- Tepthon/plugins/Search.py
+++ Tepthon/plugins/Search.py
@@ -3,13 +3,13 @@ import glob
 import random
 import yt_dlp
 from telethon import events
-from Tepthon import zedub  
+from Tepthon import zedub
 
 def get_cookies_file():
     folder_path = f"{os.getcwd()}/rcookies"
     txt_files = glob.glob(os.path.join(folder_path, '*.txt'))
     if not txt_files:
-        raise FileNotFoundError("No .txt files found in the specified folder.")
+        raise FileNotFoundError("لا توجد ملفات .txt في المجلد المحدد.")
     cookie_txt_file = random.choice(txt_files)
     return cookie_txt_file
 
@@ -37,19 +37,19 @@ async def search_song(event):
         'outtmpl': './downloads/%(title)s.%(ext)s',
     }
 
+    await event.reply("**جارِ البحث، يرجى الانتظار...**")
+    
     # البحث وتحميل الصوت
-    async with event.chat.client:
-        await event.reply("**جارِ البحث، يرجى الانتظار...**")
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            try:
-                info = ydl.extract_info(query, download=True)
-                filename = ydl.prepare_filename(info)
-                
-                # إرسال الملف
-                await event.client.send_file(event.chat_id, filename, caption=f"تحميل: {info['title']}")
-                
-            except Exception as e:
-                await event.reply(f"**حدث خطأ أثناء محاولة تحميل الصوت:** {e}")
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        try:
+            info = ydl.extract_info(query, download=True)
+            filename = ydl.prepare_filename(info)
+            
+            # إرسال الملف
+            await zedub.send_file(event.chat_id, filename, caption=f"تحميل: {info['title']}")
+            
+        except Exception as e:
+            await event.reply(f"**حدث خطأ أثناء محاولة تحميل الصوت:** {e}")
 
     # حذف الملف بعد الإرسال
     if os.path.exists(filename):
