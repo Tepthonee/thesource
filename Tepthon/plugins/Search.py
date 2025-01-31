751733a2a18542d27f5e80a570bd3b00a0702a95
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2025-01-31 11:47:55 +0300

    Update Search.py

--- Tepthon/plugins/Search.py
+++ Tepthon/plugins/Search.py
@@ -32,24 +32,38 @@ async def get_song(event):
 
     try:
         with YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(f"ytsearch:{song_name}", download=True)
+            info = ydl.extract_info(f"ytsearch:{song_name}", download=False)  # استخدم download=False للتحقق فقط
+            duration = info['duration']  # الحصول على مدة الفيديو بالدقائق
             title = info['entries'][0]['title']
-            filename = f"{title}.opus"  # تأكد من أن الامتداد صحيح
-            full_path = os.path.join(os.getcwd(), filename)
-
-            await event.edit("⎉╎ تم العثـور علـى المطلـوب، جـاري إرسال الملـف ♥️..")
-
-            # تحقق من وجود الملف حالما يتم تنزيله
-            if os.path.exists(full_path):
-                caption = "⎉╎ تم التنزيـل : @Tepthon"
-                await zedub.send_file(event.chat_id, full_path, caption=caption)
-
-                # حذف الملف بعد الإرسال فقط إذا كان قد تم إرساله بنجاح
-                os.remove(full_path)
-                await event.edit("⎉╎ تم إرسال الملف بنجاح! 🎶")
+            
+            # تحديد عتبة التحميل
+            if duration >= 1800:  # 30 دقيقة بالثواني
+                download_with_ydl = True
             else:
-                await event.edit("⎉╎ لم أتمكن من العثور على الملف الذي تم تحميله.")
-                print(f"File not found: {full_path}")  # طباعة مسار الملف المفقود
+                download_with_ydl = True  # تحميل أيضًا الصوتيات القصيرة
+
+            if download_with_ydl:
+                filename = f"{title}.opus"  # تأكد من أن الامتداد صحيح
+                full_path = os.path.join(os.getcwd(), filename)
+
+                await event.edit("⎉╎ تم العثـور علـى المطلـوب، جـاري إرسال الملـف ♥️..")
+
+                # بدء تنزيل الصوت
+                ydl_opts['download'] = True  # التأكد من أن التحميل مفعل الآن
+                with YoutubeDL(ydl_opts) as ydl:
+                    ydl.download([info['url']])  # التنزيل باستخدام URL
+
+                # تحقق من وجود الملف بعد التنزيل
+                if os.path.exists(full_path):
+                    caption = "⎉╎ تم التنزيـل : @Tepthon"
+                    await zedub.send_file(event.chat_id, full_path, caption=caption)
+
+                    # حذف الملف بعد الإرسال فقط إذا كان قد تم إرساله بنجاح
+                    os.remove(full_path)
+                    await event.edit("⎉╎ تم إرسال الملف بنجاح! 🎶")
+                else:
+                    await event.edit("⎉╎ لم أتمكن من العثور على الملف الذي تم تحميله.")
+                    print(f"File not found: {full_path}")  # طباعة مسار الملف المفقود
 
     except Exception as e:
         await event.edit(f"⎉╎ حدث خطـأ: {e}")
