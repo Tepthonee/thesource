583bb93da2ea22436085f6f05e168166ddcd4d5d
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2025-02-08 18:17:53 +0300

    Update فـحص.py

--- Tepthon/plugins/فـحص.py
+++ Tepthon/plugins/فـحص.py
@@ -1,4 +1,4 @@
-import asyncio
+import asyncio 
 import random
 import re
 import requests
@@ -6,7 +6,7 @@ import time
 import psutil
 from datetime import datetime
 from platform import python_version
-# BiLaL
+#BiLaL
 from telethon import version, events
 from telethon.tl import types, functions
 from telethon.tl.types import UserStatusOnline as onn
@@ -17,7 +17,7 @@ from telethon.errors.rpcerrorlist import (
     WebpageMediaEmptyError,
 )
 
-from . import StartTime, zedub, tepversion
+from . import StartTime, zedub, zedversion
 from ..Config import Config
 from ..helpers.functions import zedalive, check_data_base_heal_th, get_readable_time
 from ..helpers.utils import reply_id
@@ -28,7 +28,7 @@ from ..core.managers import edit_delete, edit_or_reply
 from ..core.logger import logging
 from . import BOTLOG, BOTLOG_CHATID, mention
 
-vocself = False  # تعريف المتغير كقيمته الأولية
+
 
 @zedub.zed_cmd(pattern="(تفعيل البصمه الذاتيه|تفعيل البصمه الذاتية|تفعيل البصمة الذاتيه|تفعيل البصمة الذاتية)")
 async def start_datea(event):
@@ -36,7 +36,6 @@ async def start_datea(event):
 
     if vocself:
         return await edit_or_reply(event, "**⎉╎حفظ البصمة الذاتية التلقائي 🎙**\n**⎉╎مفعلـه .. مسبقًـا ✅**")
-    
     vocself = True
     await edit_or_reply(event, "**⎉╎تم تفعيل حفظ البصمة الذاتية 🎙**\n**⎉╎تلقائيًّـا .. بنجاح ✅**")
 
@@ -44,11 +43,10 @@ async def start_datea(event):
 async def stop_datea(event):
     global vocself
     
-    if not vocself:
-        return await edit_or_reply(event, "**⎉╎حفظ البصمة الذاتية التلقائي 🎙**\n**⎉╎معطلـه .. مسبقـاً ✅**")
-    
-    vocself = False
-    await edit_or_reply(event, "**⎉╎تم تعطيل حفظ البصمة الذاتية 🎙**\n**⎉╎الان صارت مو شغالة .. ✅**")
+    if vocself:
+        vocself = False
+        return await edit_or_reply(event, "**⎉╎تم تعطيل حفظ البصمة الذاتية 🎙**\n**⎉╎الان صارت مو شغالة .. ✅**")
+    await edit_or_reply(event, "**⎉╎حفظ البصمة الذاتية التلقائي 🎙**\n**⎉╎معطلـه .. مسبقـاً ✅**")
 
 @zedub.on(events.NewMessage(func=lambda e: e.is_private and (e.audio or e.voice) and e.media_unread))
 async def sddm(event):
@@ -59,5 +57,5 @@ async def sddm(event):
         username = f"@{sender.username}" if sender.username else "لا يوجد"
         chat = await event.get_chat()
         voc = await event.download_media()
-        PM_LOGGER_GROUP_ID = ...  # تأكد من تحديد القناة المناسبة
-        await zedub.send_file(PM_LOGGER_GROUP_ID, voc, caption=f"ᯓ 𝙏𝙀𝙋𝙏𝙃𝙊𝙉 ⌁ - حفـظ البصمة الذاتية 🎙\n⋆─┄─┄─┄─┄─┄─┄─⋆\n⌔ مࢪحبـًا .. عـزيـزي 🫂\n⌔ تـم حفظ البصمة الذاتية .. تلقائيًّـا ☑️ ❝\n⌔ معلومـات المـرسـل :-\n• الاسم : {_format.mentionuser(sender.first_name , sender.id)}\n• اليوزر : {username}\n• الايدي : {sender.id}")
+       
+        await zedub.send_file(me, voc, caption=f"ᯓ 𝙏𝙀𝙋𝙏𝙃𝙊𝙉 ⌁ - حفـظ البصمـة الذاتيــة 🎙\n⋆─┄─┄─┄─┄─┄─┄─⋆\n⌔ مࢪحبـًا .. عـزيـزي 🫂\n⌔ تـم حفظ البصمة الذاتية .. تلقائيًّـا ☑️ ❝\n⌔ معلومـات المـرسـل :-\n• الاسم : {_format.mentionuser(sender.first_name , sender.id)}\n• اليوزر : {username}\n• الايدي : {sender.id}")
