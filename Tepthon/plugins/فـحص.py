b57647d576e41f513e196e094b7e109e676c359b
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2025-02-08 22:45:17 +0300

    Update فـحص.py

--- Tepthon/plugins/فـحص.py
+++ Tepthon/plugins/فـحص.py
@@ -1,37 +1,14 @@
 import asyncio 
-import random
-import re
-import requests
-import time
-import psutil
-from datetime import datetime
-from platform import python_version
-#BiLaL
-from telethon import version, events
-from telethon.tl import types, functions
-from telethon.tl.types import UserStatusOnline as onn
-from telethon.utils import get_display_name
-from telethon.errors.rpcerrorlist import (
-    MediaEmptyError,
-    WebpageCurlFailedError,
-    WebpageMediaEmptyError,
-)
-
-from . import StartTime, zedub, tepversion
-from ..Config import Config
-from ..helpers.functions import zedalive, check_data_base_heal_th, get_readable_time
-from ..helpers.utils import reply_id
-from ..core.logger import logging
+from Tepthon import zedub
+from telethon import events
+from telethon.errors.rpcerrorlist import MediaEmptyError
+from ..core.managers import edit_or_reply
 from ..helpers.utils import _format
-from ..sql_helper.globals import addgvar, delgvar, gvarstatus
-from ..core.managers import edit_delete, edit_or_reply
-from ..core.logger import logging
-from . import BOTLOG, BOTLOG_CHATID, mention
 
 # متغير للتحكم في حالة حفظ البصمة الذاتية
 vocself = True
 
-# دالة لتفعيل حفظ البصمة الذاتية
+# دالة لتفعيل حفظ الصوتيات
 @zedub.zed_cmd(pattern="(تفعيل البصمه الذاتيه|تفعيل البصمه الذاتية|تفعيل البصمة الذاتيه|تفعيل البصمة الذاتية)")
 async def start_datea(event):
     global vocself
@@ -41,7 +18,7 @@ async def start_datea(event):
     vocself = True
     await edit_or_reply(event, "⎉╎تم تفعيل حفظ البصمة الذاتية 🎙\n⎉╎تلقائيًّـا .. بنجاح ✅")
 
-# دالة لإيقاف حفظ البصمة الذاتية
+# دالة لإيقاف حفظ الصوتيات
 @zedub.zed_cmd(pattern="(ايقاف البصمه الذاتيه|ايقاف البصمه الذاتية|ايقاف البصمة الذاتيه|ايقاف البصمة الذاتية)")
 async def stop_datea(event):
     global vocself
@@ -55,14 +32,16 @@ async def stop_datea(event):
 @zedub.on(events.NewMessage(func=lambda e: e.is_private and (e.audio or e.voice) and e.media_unread))
 async def sddm(event):
     global vocself
-  
+
     if vocself:
         sender = await event.get_sender()
         username = f"@{sender.username}" if sender.username else "لا يوجد"
         chat = await event.get_chat()
-        voc = await event.download_media()  # تحميل الوسائط (الصوتيات)
-       
-        await zedub.send_file("me", voc, caption=f"[ᯓ 𝙏𝙀𝙋𝙏𝙃𝙊𝙉 ⌁ - حفـظ البصمـة الذاتيــة 🎙\n⋆─┄─┄─┄─┄─┄─┄─⋆\n⌔ مࢪحبـًا .. عـزيـزي 🫂\n⌔ تـم حفظ البصمة الذاتية .. تلقائيًّـا ☑️ ❝\n⌔ معلومـات المـرسـل :-\n• الاسم : {_format.mentionuser(sender.first_name, sender.id)}\n• اليوزر : {username}\n• الايدي : {sender.id}")
+        try:
+            voc = await event.download_media()  # تحميل الوسائط (الصوتيات)
+            await zedub.send_file("me", voc, caption=f"[ᯓ 𝙏𝙀𝙋𝙏𝙃𝙊𝙉 ⌁ - حفـظ البصمـة الذاتية 🎙\n⋆─┄─┄─┄─┄─┄─┄─⋆\n⌔ مࢪحبـًا .. عـزيـزي 🫂\n⌔ تـم حفظ البصمة الذاتية .. تلقائيًّـا ☑️ ❝\n⌔ معلومـات المـرسـل :-\n• الاسم : {_format.mentionuser(sender.first_name , sender.id)}\n• اليوزر : {username}\n• الايدي : {sender.id}")
+        except MediaEmptyError:
+            await edit_or_reply(event, "⎉╎حدث خطأ أثناء حفظ الصوتية. يرجى المحاولة مجددًا.")
 
 # ---------- تنبيه -----------
 # تأكد من وجود متطلبات تشغيل البرنامج والإعدادات الصحيحة لـ Telethon
