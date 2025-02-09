3e58027061f3fc64bb9a15757c783d2c62e39af1
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2025-02-09 15:30:46 +0300

    Update فـحص.py

--- Tepthon/plugins/فـحص.py
+++ Tepthon/plugins/فـحص.py
@@ -1,6 +1,6 @@
-import asyncio 
-from Tepthon import zedub
+import asyncio
 from telethon import events
+from Tepthon import zedub
 from telethon.errors.rpcerrorlist import MediaEmptyError
 from ..core.managers import edit_or_reply
 from ..helpers.utils import _format
@@ -8,7 +8,7 @@ from ..helpers.utils import _format
 # متغير للتحكم في حالة حفظ البصمة الذاتية
 vocself = True
 
-# دالة لتفعيل حفظ الصوتيات
+# دالة لتفعيل حفظ الصوتيات الذاتية
 @zedub.zed_cmd(pattern="(تفعيل البصمه الذاتيه|تفعيل البصمه الذاتية|تفعيل البصمة الذاتيه|تفعيل البصمة الذاتية)")
 async def start_datea(event):
     global vocself
@@ -18,7 +18,7 @@ async def start_datea(event):
     vocself = True
     await edit_or_reply(event, "⎉╎تم تفعيل حفظ البصمة الذاتية 🎙\n⎉╎تلقائيًّـا .. بنجاح ✅")
 
-# دالة لإيقاف حفظ الصوتيات
+# دالة لإيقاف حفظ الصوتيات الذاتية
 @zedub.zed_cmd(pattern="(ايقاف البصمه الذاتيه|ايقاف البصمه الذاتية|ايقاف البصمة الذاتيه|ايقاف البصمة الذاتية)")
 async def stop_datea(event):
     global vocself
@@ -28,7 +28,7 @@ async def stop_datea(event):
         return await edit_or_reply(event, "⎉╎تم تعطيل حفظ البصمة الذاتية 🎙\n⎉╎الان صارت مو شغالة .. ✅")
     await edit_or_reply(event, "⎉╎حفظ البصمة الذاتية التلقائي 🎙\n⎉╎معطلـه .. مسبقـاً ✅")
 
-# دالة للاستماع للرسائل الصوتية الجديدة وحفظها
+# دالة للاستماع للرسائل الصوتية الذاتية الجديدة وحفظها
 @zedub.on(events.NewMessage(func=lambda e: e.is_private and (e.audio or e.voice) and e.media_unread))
 async def sddm(event):
     global vocself
@@ -36,7 +36,6 @@ async def sddm(event):
     if vocself:
         sender = await event.get_sender()
         username = f"@{sender.username}" if sender.username else "لا يوجد"
-        chat = await event.get_chat()
         try:
             voc = await event.download_media()  # تحميل الوسائط (الصوتيات)
             await zedub.send_file("me", voc, caption=f"[ᯓ 𝙏𝙀𝙋𝙏𝙃𝙊𝙉 ⌁ - حفـظ البصمـة الذاتية 🎙\n⋆─┄─┄─┄─┄─┄─┄─⋆\n⌔ مࢪحبـًا .. عـزيـزي 🫂\n⌔ تـم حفظ البصمة الذاتية .. تلقائيًّـا ☑️ ❝\n⌔ معلومـات المـرسـل :-\n• الاسم : {_format.mentionuser(sender.first_name , sender.id)}\n• اليوزر : {username}\n• الايدي : {sender.id}")
