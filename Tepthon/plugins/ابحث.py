e59ca176d69e589032c14541a927ddee5f067b7f
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2025-02-09 21:16:45 +0300

    Update ابحث.py

--- Tepthon/plugins/ابحث.py
+++ Tepthon/plugins/ابحث.py
@@ -9,14 +9,14 @@ import random
 
 def get_cookies_file():
     # تأكد من تعديل مسار المجلد حسب احتياجك
-    folder_path = f"{os.getcwd()}/cookies"
+    folder_path = f"{os.getcwd()}/rcookies"
     txt_files = glob.glob(os.path.join(folder_path, '*.txt'))
     if not txt_files:
         raise FileNotFoundError("No .txt files found in the specified folder.")
     cookie_txt_file = random.choice(txt_files)
     return cookie_txt_file
 
-@zedub.on(events.NewMessage(pattern=".يوت"))
+@zedub.on(events.NewMessage(pattern=".يوتيوب"))
 async def srchDl(e):
     try:
         txt = e.raw_text.split()
