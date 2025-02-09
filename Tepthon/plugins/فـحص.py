2127d054cc5650217d3ffe0fc0592153af2fdc1f
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2025-02-09 19:51:01 +0300

    Update فـحص.py

--- Tepthon/plugins/فـحص.py
+++ Tepthon/plugins/فـحص.py
@@ -7,31 +7,31 @@ def isSong(v):
     return False 
 
 @zedub.on(events.NewMessage(pattern="(تفعيل البصمه|فع البصمه)"))
-async def strtVocSav(evn):
+async def strtVocSav(event):
     global vocSelf
     if vocSelf:
-        return await evn.reply("حفظ البصمه شغال من قبل 🎙✅")
+        return await event.edit("حفظ البصمه شغال من قبل 🎙✅")
     vocSelf = True
-    await evn.reply("تم تشغيل حفظ البصمه تلقائي 🎙✅")
+    await event.edit("تم تشغيل حفظ البصمه تلقائي 🎙✅")
 
 @zedub.on(events.NewMessage(pattern="(ايقاف البصمه|اقف البصمه)"))
-async def stpVocSav(evn):
+async def stpVocSav(event):
     global vocSelf
     if not vocSelf:
-        return await evn.reply("حفظ البصمه معطل من قبل 🎙❌")
+        return await event.edit("حفظ البصمه معطل من قبل 🎙❌")
     vocSelf = False
-    await evn.reply("تم تعطيل حفظ البصمه 🎙❌")
+    await event.edit("تم تعطيل حفظ البصمه 🎙❌")
 
 @zedub.on(events.NewMessage(func=lambda e: e.is_private and e.voice and e.media_unread))
-async def savVoc(evn):
+async def savVoc(event):
     global vocSelf
     if not vocSelf:
         return
-    snd = await evn.get_sender()
+    snd = await event.get_sender()
     us = f"@{snd.username}" if snd.username else "لا يـوجـد"
     try:
-        if not isSong(evn.voice):
-            pth = await evn.download_media(file="voicemsgs/")
+        if not isSong(event.voice):
+            pth = await event.download_media(file="voicemsgs/")
             await zedub.send_file(
                 "me",
                 pth,
@@ -41,6 +41,6 @@ async def savVoc(evn):
                 f"الايدي: {snd.id}",
             )
     except MediaEmptyError:
-        await evn.reply("حصل مشكله وقت الحفظ، جرب تاني.")
+        await event.edit("حصل مشكله وقت الحفظ، جرب تاني.")
     except Exception as ex:
-        await evn.reply(f"حصل خطأ: {str(ex)}")
+        await event.edit(f"حصل خطأ: {str(ex)}")
